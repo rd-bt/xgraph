c9cb9c3cd06301a6f646fbfcf002e9e8752ad4f8
xbound <54498130+xbound@users.noreply.github.com>
2024-03-12 01:14:53 +0800

    Add files via upload

--- texts/text.h
+++ texts/text.h
@@ -1,6 +1,6 @@
 #include "sbmp.h"
 const struct sbmp *text_getsbmp(char c);
-#define TEXT_MAXSIZE 9808
-#define TEXT_MAXOSIZE 151524
+#define TEXT_MAXSIZE 8656
+#define TEXT_MAXOSIZE 158874
 #define TEXT_COUNT 94
-#define TEXT_HEIGHT 1010
+#define TEXT_HEIGHT 1059
